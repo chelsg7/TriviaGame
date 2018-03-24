5c09b8dbcf354bc4c459f6dadee4d92caabd5dc5
Chelsea G <chelseagieck@Chelseas-MacBook-Pro.local>
2018-03-24 18:50:56 -0400

    working timer and reset

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,5 @@
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
-
 var timerClear = 00 + ":" + 00;
-
-
-
-
-var start = function(duration, display) {
-    var start = Date.now(),
-    diff,
-    minutes,
-    seconds;
-    var timer = function () {
-        diff = duration - (
-            ((Date.now() - start) / 1000) | 0);
-            minutes = (diff / 60) | 0;
-            seconds = (diff % 60) | 0;
-            
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
-            
-            display.textContent = minutes + ":" + seconds; 
-            
-            if (diff <= 0) {
-                start = Date.now() + 1000;
-            }
-        };
-        timer();
-        setInterval(timer, 1000);
-    };
-
-var questions = [
+var myQuestions = [
 	{
 		question: 'In the "Harry Potter" book series, He Who Must Not Be Named" is also known as??',
 		answers: {
@@ -105,118 +73,154 @@ var questions = [
 {
 		question: "In the state of Texas, it's illegal to do what?",
 		answers: {
-			a: "Swear in front of a corpse",
-			b: "Milk another man's goat",
-            c: 'Use a mousetrap without a hunting permit',
-            d: 'Teach other people what polygamy is'
+			a: "It is illegal for one to shoot a buffalo from the second story of a hotel.",
+			b: "It is illegal to milk another person’s goat.",
+            c: 'It is a felony to steal more than $1000 of grease',
+            d: 'A man may not seduce a woman by lying, and claiming he will marry her.'
         },
         correctAnswer: 'a'
     },
 ];
 
-var showResults = function (questions, quizContainer, resultsContainer){
-        
-    // gather answer containers from our quiz
-    var answerContainers = quizContainer.querySelectorAll('.answers');
-    
-    // keep track of user's answers
-    var userAnswer = '';
-    var numCorrect = 0;
-    
-    // for each question...
-    for(var i=0; i<questions.length; i++){
 
-        // find selected answer
-        userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
-        
-        // if answer is correct
-        if(userAnswer===questions[i].correctAnswer){
-            // add to the number of correct answers
-            numCorrect++;
+var start = function(duration, display) {
+    var start = Date.now(),
+    diff,
+    minutes,
+    seconds;
+    var timer = function () {
+        diff = duration - (
+            ((Date.now() - start) / 1000) | 0);
+            minutes = (diff / 60) | 0;
+            seconds = (diff % 60) | 0;
             
-            // color the answers green
-            answerContainers[i].style.color = 'green';
-        }
-        // if answer is wrong or blank
-        else{
-            // color the answers red
-            answerContainers[i].style.color = 'red';
-        }
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+            
+            display.textContent = minutes + ":" + seconds; 
+            
+            if (diff <= 0) {
+                start = Date.now() + 1000;
+            }
+        };
+        timer();
+        setInterval(timer, 1000);
+    };
+
+    var reset = function(){
+        var zero = 0;
+        clearInterval(timer);
+        display = document.querySelector('#timer');
+        start(zero, display);
     }
 
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = numCorrect + ' / ' + questions.length;
-};
+    var quizContainer = document.getElementById('quiz');
+    var resultsContainer = document.getElementById('results');
+    var submitButton = document.getElementById('submit');
 
-var  generateQuiz = function(questions, quizContainer, resultsContainer, submitButton){
-    
-    var showQuestions = function (questions, quizContainer){
-        // we'll need a place to store the output and the answer choices
-        var output = [];
-        var answers;
 
+    var showResults = function(questions, quizContainer, resultsContainer){
+            
+        // gather answer containers from our quiz
+        var answerContainers = quizContainer.querySelectorAll('.answers');
+        
+        // keep track of user's answers
+        var userAnswer = '';
+        var numCorrect = 0;
+        
         // for each question...
         for(var i=0; i<questions.length; i++){
+
+            // find selected answer
+            userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
             
-            // first reset the list of answers
-            answers = [];
-
-            // for each available answer...
-            for(letter in questions[i].answers){
-
-                // ...add an html radio button
-                answers.push(
-                    '<label>'
-                        + '<input type="radio" name="question'+i+'" value="'+letter+'">'
-                        + "  "  + letter + ' : '
-                        + questions[i].answers[letter]
-                    + '</label>'
-                );
+            // if answer is correct
+            if(userAnswer===questions[i].correctAnswer){
+                // add to the number of correct answers
+                numCorrect++;
+                
+                // color the answers green
+                answerContainers[i].style.color = 'lightgreen';
             }
-
-            // add this question and its answers to the output
-            output.push(
-                '<div class="question card mt-5 mb-2 mx-auto text-center col-12">' + "   " + questions[i].question + '</div>'
-                + '<div class="answers card px-5">' +  answers.join('') + '</div>'
-            );
+            // if answer is wrong or blank
+            else{
+                // color the answers red
+                answerContainers[i].style.color = 'red';
+            }
         }
 
-        // finally combine our output list into one string of html and put it on the page
-        quizContainer.innerHTML = output.join('');
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
     };
+   
     
-
-    // show questions right away
-    showQuestions(questions, quizContainer);
+    function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
     
-    // on submit, show results
-    submitButton.onclick = function(){
-        showResults(questions, quizContainer, resultsContainer);
-    }
+        function showQuestions(questions, quizContainer){
+            // we'll need a place to store the output and the answer choices
+            var output = [];
+            var answers;
+    
+            // for each question...
+            for(var i=0; i<questions.length; i++){
+                
+                // first reset the list of answers
+                answers = [];
+    
+                // for each available answer...
+                for(letter in questions[i].answers){
+    
+                    // ...add an html radio button
+                    answers.push(
+                        '<label>'
+                            + '<input type="radio" name="question'+i+'" value="'+letter+'">' + " "
+                            + letter + ' : '
+                            + questions[i].answers[letter]
+                        + '</label>'
+                    );
+                }
+    
+                // add this question and its answers to the output
+                output.push(
+                    '<div class="question card card-head bg-warning py-2 mt-5 text-center">' + questions[i].question + '</div>'
+                    + '<div class="answers card card-body border-warning">' + answers.join('') + '</div>'
+                );
+            }
+    
+            // finally combine our output list into one string of html and put it on the page
+            quizContainer.innerHTML = output.join('');
+        };
 
-}
+        // show questions right away
+        showQuestions(myQuestions, quizContainer);
+        
+        // on submit, show results
+        submitButton.onclick = function(){
+            showResults(myQuestions, quizContainer, resultsContainer);
+        }
+    };
 
 
 $( document ).ready(function() {
     console.log( "ready!" );
-    
-    
-    $('#start').on("click", function() {
-        console.log("start timer!")
-        var twoMinutes = 60 * 2;
-        
-        display = document.querySelector('#timer');
-        start(twoMinutes, display);
-        generateQuiz(questions, quizContainer, resultsContainer, submitButton);
-        setInterval(
-            function(){
-                alert("Time is up!");
-                showResults(questions, quizContainer, resultsContainer);
-            }, 120000);
-            clearInterval();
-            $('#timer').html(timerClear);
-    }
-);
-
-
 });
+
+
+$('#start').on("click", function() {
+    console.log("start timer!")
+    var twoMinutes = 60 * 2;
+    
+    display = document.querySelector('#timer');
+    start(twoMinutes, display);
+    generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+    setInterval(
+        function(){
+            alert("Time is up!");
+            showResults(myQuestions, quizContainer, resultsContainer);
+            reset();
+    }, 120000);
+    clearInterval();
+
+        
+}
+);
